d9cc666db27c6c2234562782ce599eb62af8dd8e
ztengw <165560023+ztengw@users.noreply.github.com>
2026-06-08 22:47:20 -0700

    Add hamburger navigation functionality in email.js

---
+++ js/email.js
@@ -0,0 +1,15 @@
+// Hamburger navigation
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+if (hamburger && navigation) {
+  hamburger.addEventListener("click", function () {
+    navigation.classList.toggle("active");
+  });
+}
+
+function sendMail(){
+    let parms = {
+        
+    }
+}
